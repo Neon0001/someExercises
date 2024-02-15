f85d8c819d9f9e1825f3cbd8d54c85c9e7978751
Roger <64852875+Neon0001@users.noreply.github.com>
2024-02-15 19:23:30 -0300

    24hr time converter

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-def plusMinus(arr):
-    positive = 0
-    negativeRatio = 0
-    zero = 0
+def lonelyinteger(a):
+    unique = 0
+    for numbers in a:
+        if a.count(numbers) == 1:
+            unique = numbers
+    return unique
 
-    for numbers in arr:
-        positive += numbers > 0
-        negativeRatio += numbers < 0
-        zero += numbers == 0
-
-    arr_length = len(arr)
-
-    positiveF = positive / arr_length
-    negativeF = negativeRatio / arr_length
-    zeroF = zero / arr_length
-
-    print("{:.6f}".format(positiveF) + "\n" + "{:.6f}".format(negativeF) + "\n" + "{:.6f}".format(zeroF))
-
-
-plusMinus([2, 4, 5, 2, 5, 2, -2, -3, -4, 0, 0, 0])
+print(lonelyinteger([1,2,3,4,3,2,1]))
